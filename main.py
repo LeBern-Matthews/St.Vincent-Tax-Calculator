87b9ec83f40cc12c2444ca776e4ebe5cd736ebc8
LeBern Matthews <128014055+LeBern-Matthews@users.noreply.github.com>
2024-11-09 13:08:51 -0400

    Created a main function, propper formatting of results, minute error fixes

--- main.py
+++ main.py
@@ -1,57 +1,69 @@
-STANDARD_DEDUCTION=25000
-
-pay_interval=input("How do you get payed? \ne.g monthly, weekly, biweekly: ").lower()
-pay=float(input("Enter your pay for the above method:"))
-
-
 def Calculate_yearly_multiple(pay_interval:str)->int:
-    while True:
-        match (pay_interval):
-            case "monthly":
-                yearly_multiple=12
-            case "biweekly":
-                yearly_multiple=26
-            case "weekly":
-                yearly_multiple=52
-            case default:
-                print("Please enter a valid interval of payment of ")
-                pay_interval=input("How do you get payed? \ne.g monthly, weekly, biweekly: ").lower()
+    match (pay_interval):
+        case "monthly":
+            yearly_multiple=12
+        case "biweekly":
+            yearly_multiple=26
+        case "weekly":
+            yearly_multiple=52
+        case default:
+            print("Please enter a valid interval of payment of ")
+            pay_interval=input("How do you get payed? \ne.g monthly, weekly, biweekly: ").lower()
+            yearly_multiple=Calculate_yearly_multiple(pay_interval)
     return yearly_multiple
 
-def Calculate_yearly_salery(pay_interval:str,pay: float)-> float:
+def Calculate_yearly_salary(pay_interval:str,pay: float)-> float:
     """
     calculates the gross yearly pay
     
     Parameters:
         pay_interval(str): How often the user gets payed
-        pay(str): How much they are payed based on pay_interval
+        pay(float): How much they are payed based on pay_interval
         
     Returns:
-        float: A floating point number with that is the yearly salery
+        float: A floating point number with that is the yearly salary
     """
-        
+
     return pay*Calculate_yearly_multiple(pay_interval)
     
-def Calculate_finances(income: float, currency:str, yearly_multiple:int):
-    chargible_income=income-STANDARD_DEDUCTION
-
-    print(f"The chargible income is {currency}{chargible_income}")
+def Calculate_tax_per_interval(income: float, yearly_multiple:int, chargible_income: float):
     
     if chargible_income <=5000:
-        tax=0.1*chargible_income
-        return tax
+        tax= 0.1 * (chargible_income/yearly_multiple)
+        
     elif chargible_income<=10000:
         excess=chargible_income-5000
-        excess_per=excess/yearly_multiple
-        print(excess_per)
-        tax=(500/yearly_multiple)+(0.2*excess_per)
-        return tax
+        tax=(1/yearly_multiple)*(500+0.2*excess)
+        
     elif chargible_income>10000:
         excess=chargible_income-10000
-        excess_per=excess/yearly_multiple
-        print(excess_per)
-        tax=(1500/yearly_multiple)+(0.28*excess_per)
-        return tax
+        tax=(1/yearly_multiple)*(1500+0.28*excess)
+        
     
+    print("Calculate tax =",tax)
+    return '{:.2f}'.format(tax)
+
+def Calculate_financess(yearly_salary, chargible_income,yearly_multiple,pay_interval,tax_per_interval, currency):
     
-print(Calculate_finances(Calculate_yearly_salery(pay_interval,pay), "$EC", Calculate_yearly_multiple(pay_interval)))
+    print(f"Yearly gross salary is:{currency}{yearly_salary}")
+    print(f"Yearly chargible income is:{currency}{chargible_income}")
+    print(f"{pay_interval.capitalize()} chargible income is:{currency}{chargible_income/yearly_multiple}")
+    print(f"{pay_interval.capitalize()} salary is : {currency}{yearly_salary/yearly_multiple}")
+    print(f"{pay_interval.capitalize()} tax is : {currency}{tax_per_interval}")
+    print(f"Yealry tax is : {currency}{float(tax_per_interval)*yearly_multiple}")
+    
+def main():
+    STANDARD_DEDUCTION=25000
+
+    pay_interval=input("How do you get payed? \ne.g monthly, weekly, biweekly: ").lower()
+    pay=float(input("Enter your pay for the above method:"))
+    
+    yearly_salary=Calculate_yearly_salary(pay_interval,pay)
+    chargible_income=yearly_salary-STANDARD_DEDUCTION
+    
+    yearly_multiple=Calculate_yearly_multiple(pay_interval)
+    tax_per_interval=Calculate_tax_per_interval(yearly_salary,yearly_multiple, chargible_income)
+    
+    Calculate_financess(yearly_salary,chargible_income,yearly_multiple ,pay_interval,tax_per_interval,"EC$")
+    
+main()
